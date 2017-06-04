3207087e2610a84466eb8e00c3155600a3b4f95e
박효완 <qkrgy1206@naver.com>
2017-06-05 01:52:02 +0900

    treeview

---
+++ WindowsExplorer/WindowsExplorer/HeaderToImageConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using System.Globalization;
+
+namespace WindowsExplorer
+{
+    [ValueConversion(typeof(string), typeof(bool))]
+    public partial class HeaderToImageConverter : IValueConverter
+    {
+        public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((value as string).Contains(@"\"))
+            {
+                Uri uri = new Uri("http://findicons.com/files/icons/326/pry_system/128/aluport_extreme_drive.png");
+                BitmapImage source = new BitmapImage(uri);
+                return source;
+            }
+            else
+            {
+                Uri uri = new Uri("http://findicons.com/files/icons/730/soft/128/folder.png");
+                BitmapImage source = new BitmapImage(uri);
+                return source;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("Cannot convert back");
+        }
+    }
+
+
+}
---
+++ WindowsExplorer/WindowsExplorer/MainExplorer.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WindowsExplorer
+{
+    /// <summary>
+    /// MainExplorer.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainExplorer : UserControl
+    {
+        DirectoryInfo info;
+        private object dummyNode = null;
+        public MainExplorer()
+        {
+            InitializeComponent();
+            info = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+        }
+        public string SelectedPath { get; set; }
+        private void treeView_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (string itemName in Directory.GetLogicalDrives())
+            {
+                TreeViewItem item = new TreeViewItem();
+                item.Header = itemName;
+                item.Tag = itemName;
+                item.FontWeight = FontWeights.Normal;
+                item.Items.Add(dummyNode);
+                item.Expanded += new RoutedEventHandler(folder_Expanded);
+                treeView.Items.Add(item);
+            }
+        }
+
+        void folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = sender as TreeViewItem;
+            if (item.Items.Count == 1 && item.Items[0] == dummyNode)
+            {
+                item.Items.Clear();
+                foreach (string s in Directory.GetDirectories(item.Tag.ToString()))
+                {
+                    TreeViewItem subitem = new TreeViewItem();
+                    subitem.Header = s.Substring(s.LastIndexOf("\\") + 1);
+                    
+                    subitem.Tag = s;
+                    subitem.FontWeight = FontWeights.Normal;
+
+                    subitem.Items.Add(dummyNode);
+                    subitem.Expanded += new RoutedEventHandler(folder_Expanded);
+                    item.Items.Add(subitem);
+                }
+
+            }
+        }
+
+        private void treeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            TreeView tree = sender as TreeView;
+            TreeViewItem temp = tree.SelectedItem as TreeViewItem;
+
+            if (temp == null)
+                return;
+            SelectedPath = "";
+            string temp1 = "";
+            string temp2 = "";
+            while (true)
+            {
+                temp1 = temp.Header.ToString();
+                if (temp1.Contains(@"\"))
+                {
+                    temp2 = "";
+                }
+                SelectedPath = temp1 + temp2 + SelectedPath;
+                if (temp.Parent.GetType().Equals(typeof(TreeView)))
+                {
+                    break;
+                }
+                temp = ((TreeViewItem)temp.Parent);
+                temp2 = @"\";
+            }
+            //show user selected path
+            pathTextBox.Text = SelectedPath;
+        }
+        
+    }
+}
